bf7d297149443dd19087ff67167874c2b5ddf4b0
Raúl Prada <r.alejandro989@gmail.com>
2026-02-05 01:13:21 -0300

    WIP: Adding set_filter_options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :set_filter_options, only: [:index]
 
   def index
     @products = ProductFilterService.new(filter_params).call
@@ -25,5 +26,14 @@ class ProductsController < ApplicationController
     params.permit(:query, :category, :brand, :min_price, :max_price, :sort_by, :in_stock_only, :price_range)
   end
 
-
+  def set_filter_options
+    @categories = Product.all_categories
+    @brands = Product.all_brands
+    @price_ranges = [
+      { label: 'Menos de $50', min: 0, max: 50 },
+      { label: '$50 - $100', min: 50, max: 100 },
+      { label: '$100 - $200', min: 100, max: 200 },
+      { label: 'Más de $200', min: 200, max: 999999 }
+    ]
+  end
 end
